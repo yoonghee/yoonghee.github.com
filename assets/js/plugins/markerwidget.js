2dce6035ea4bf476088a57cc46f399379acf1451
yoonghee <youghee2014@gmail.com>
2021-02-10 10:50:45 +0900

    Add setting navigation, js, scss, admin, domain, makerwidget, disqus

---
+++ assets/js/plugins/markerwidget.js
@@ -0,0 +1,12 @@
+(function(d, h, m){
+    var js, fjs = d.getElementsByTagName(h)[0];
+    if (d.getElementById(m)){return;}
+    js = d.createElement(h); js.id = m;
+    js.onload = function(){
+        window.makerWidgetComInit({
+        position: "left",          
+        widget: "ro9dtdpokabxppae-gfr7wabqtzuvt9h8-d7f7ygs6axebcis4"                
+    })};
+    js.src = "https://makerwidget.com/js/embed.js";
+    fjs.parentNode.insertBefore(js, fjs)
+}(document, "script", "dhm"))
